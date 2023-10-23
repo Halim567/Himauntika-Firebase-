c28693cd540c166c55d3070be47ac8d680a70f8d
Halim567 <etzergd916@gmail.com>
2023-10-23 21:40:15 +0700

    type any message here

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,6 +2,8 @@ import { signInAnonymously } from 'firebase/auth';
 import type { LayoutServerLoad } from './$types';
 import { auth } from '$lib/firebase_app/database';
 
+export const prerender = true;
+
 export const load: LayoutServerLoad = async ({ locals }) => {
     await signInAnonymously(auth);
     return {
